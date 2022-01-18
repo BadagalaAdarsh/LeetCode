e4de9b7ee51b5199e7aeeb2f890d12cdf9ae692c
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-01-18 23:21:34 +0530

    Time: 92 ms (43.27%), Space: 48.4 MB (49.59%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1){
+            return true;
+        }
+        
+        int can_reach = 0;
+        for(int i = 0; i <= can_reach; i++){
+            if (i == n-1){
+                return true;
+            }
+            can_reach = max(can_reach, i + nums[i]);
+        }
+        return false;        
+    }
+};
