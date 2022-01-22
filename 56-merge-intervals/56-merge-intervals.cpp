435864466c83b92254b74c5ad0096d7e48793ee2
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-01-23 00:48:40 +0530

    Time: 168 ms (5.06%), Space: 47.2 MB (5.12%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+vector<vector<int>> res;
+
+
+    // sorting the 2d array based on the first number which uses lamda function
+    sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+        return a[0] < b[0];
+    });
+
+    res.emplace_back(intervals[0]);
+
+    // traverse the intervals vector and add to result based on the overlapping condition
+
+    for(int i =1 ; i < (int)intervals.size(); i++){
+        
+        // if first number of next interval is smaller or equal to the last number of the previous interval
+        if(intervals[i][0] <= res.back()[1]){
+            // current last number will the max of current last and next last number
+            res.back()[1] = max(res.back()[1], intervals[i][1]);
+        }
+        else{
+            // else directly add the next interval to the result
+            res.emplace_back(intervals[i]);
+        }
+    }
+    // return the result
+    return res;
+    }
+};
