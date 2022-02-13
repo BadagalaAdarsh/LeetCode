f6b96779fc858e8b1074ff275290228419249faf
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-02-13 22:41:10 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (85.44%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void function(int start, int end, vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans) {
+        
+        if(start == end) {
+            ans.push_back(temp);
+            return;
+        }
+        
+        temp.push_back(nums[start]);
+        function(start+1, end, nums, temp, ans);
+        temp.pop_back();
+        function(start+1, end, nums, temp, ans);
+        
+    }
+    
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        
+        vector<int> temp;
+        function(0, nums.size(), nums, temp, ans);
+        
+        return ans;
+    }
+};
