9a8127867e36bd54200a2b78c3190e3fa172d6f7
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-02-22 22:40:47 +0530

    Time: 4 ms (47.16%), Space: 8.6 MB (98.48%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--) {
+            if(++digits[i]<10)
+                return digits;
+            digits[i]=0;
+        }
+        digits[0]=1;
+        digits.push_back(0);
+        return digits;
+    }
+};
