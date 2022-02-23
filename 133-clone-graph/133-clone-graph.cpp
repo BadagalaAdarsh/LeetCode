6d65fb8179f940bad3d6c13efc1179c90bc66fef
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-02-23 23:11:11 +0530

    Time: 11 ms (35.55%), Space: 9.1 MB (9.88%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        
+        if(node == NULL)
+            return node;
+        
+        unordered_map<int, Node*> visited;
+        
+        return clone_node(node, visited);
+        
+    }
+    
+    Node* clone_node(Node* node, unordered_map<int, Node*> & visited){
+        Node* new_node = new Node(node->val);
+        
+        visited.insert({node->val, new_node});
+        
+        for(Node* n: node->neighbors){
+            auto it = visited.find(n->val);
+            
+            if(it == visited.end()){
+                Node* cn = clone_node(n, visited);
+                new_node->neighbors.push_back(cn);
+            }
+            else{
+                new_node->neighbors.push_back(it->second);
+            }
+        }
+        
+        return new_node;
+    }
+};
