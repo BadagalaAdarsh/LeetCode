4ee31d63de8e335bcc35b53e8ae6fe915b565c57
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-03-04 21:56:20 +0530

    Time: 8 ms (31.43%), Space: 10 MB (5.24%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        unordered_set<int> uset(arr.begin(),arr.end());
+        int max = *max_element(arr.begin(),arr.end());
+        int min = *min_element(arr.begin(),arr.end());
+        int diff = (max-min)/(arr.size()-1);
+        for(int i=1;i<arr.size();i++){
+            if(!uset.count(min + i * diff)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
