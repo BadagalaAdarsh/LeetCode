28c67bb2d5b9878d4222b914cdffd095ce7c0e02
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-03-08 23:01:42 +0530

    Time: 92 ms (98.96%), Space: 37.8 MB (87.06%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,8 +1,9 @@
 class Solution {
-public:
-    int get(int x, vector<int> &parent){
+private:
+    int get(int x, vector<int>& parent){
         return parent[x] = parent[x] == x? x: get(parent[x], parent);
     }
+    
     void merge(int l, int r, vector<int> &parent, vector<int>&rank){
         l = get(l, parent);
         r = get(r, parent);
@@ -11,24 +12,34 @@ public:
         else parent[l] = r;
         return ;
     }
+public:
     int makeConnected(int n, vector<vector<int>>& connections) {
+        
         int totalEdges = connections.size();
-        if(totalEdges < n-1) return -1;
+        
+        // a graph with n nodes needs atleast n -1 edges to get all connected
+        if(totalEdges < n - 1) return -1;
+        
         vector<int> parent(n);
         vector<int> rank(n, 0);
-        for(int i=0; i<n; i++){
+        
+        for(int i = 0; i < n; i++) {
             parent[i] = i;
         }
-        for(int i=0; i<connections.size(); i++){
+        
+        for(int i = 0; i < connections.size(); i++) {
             int u = connections[i][0];
             int v = connections[i][1];
-            merge(u,v, parent, rank);
+            
+            merge(u, v, parent, rank);
         }
-        int count = 0;
-        for(int i=0; i<n; i++){
-            if(parent[i] == i) count++;
-        }
-        return count-1;
         
+        int count = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(parent[i]  == i) count++;
+        }
+        
+        return count - 1;
     }
 };
