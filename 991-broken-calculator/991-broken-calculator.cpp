75050e2846a360a5b30977e012994421bbf22eb9
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-03-24 00:40:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    
+public:
+    int brokenCalc(int startValue, int target) {
+        
+        if(startValue >= target) return startValue - target;
+        
+        if(target % 2 == 0)
+            return 1 + brokenCalc(startValue, target / 2);
+        
+        return 1 + brokenCalc(startValue, target + 1);
+    }
+};
