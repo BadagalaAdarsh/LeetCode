ee406c596ead958781a40d608af1ea818287a806
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-04-14 23:38:41 +0530

    Time: 11 ms (34.28%), Space: 5.9 MB (27.78%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int rows = 1;
+        while(n > 0) {     
+            rows++;       
+            n -= rows;     
+        }
+        return rows - 1;  
+    }
+};
