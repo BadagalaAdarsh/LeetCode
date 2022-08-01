7b6d17b3ff4960dd0b1caf0807b96f1ac4f3f7c5
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-08-01 22:39:26 +0530

    Create 62-unique- paths.cpp

---
+++ 62-unique-paths/62-unique- paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        // its a combinatrics solution
+        // answer nCr
+        
+        int N = n + m -2;
+        int r = m - 1;
+        double res = 1;
+        
+        for(int i = 1; i <= r; i++) {
+            res = res * ( N- r+ i) / i;
+        }
+        
+        return (int)res;
+        
+    }
+};
