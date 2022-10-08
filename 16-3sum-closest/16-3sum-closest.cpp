28880328ba24520496c7b2717acfa727c9930689
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-10-08 22:43:45 +0530

    Create 16-3sum-closest.cpp

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,24 @@
+int threeSumClosest(vector<int>& nums, int target) {
+        int n=nums.size(),closestValue=-1,minValue=INT_MAX;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=n-3;i++){
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                if(abs(nums[i]+nums[l]+nums[r]-target)<minValue){
+                    minValue=abs(nums[i]+nums[l]+nums[r]-target);
+                    closestValue=nums[i]+nums[l]+nums[r];
+                }
+                if(nums[i]+nums[l]+nums[r]<target){
+                    l++;
+                }
+                else if(nums[i]+nums[l]+nums[r]>target){
+                    r--;
+                }
+                else{
+                    return target;
+                }
+            }
+        }
+        return closestValue;
+    }
