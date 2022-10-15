9f04e657972b7d7206df9d01acd9280035c5c852
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-10-15 10:23:25 +0530

    Update 236-lowest-common-ancestory-of-a-binary-tree.cpp

--- 236-lowest-common-ancestory-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestory-of-a-binary-tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // base case 
+        
         if(root == NULL || root == p || root == q) {
             return root;
         }
